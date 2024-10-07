08a50a4e3de5f74a88f1b5438349a58fe69810f8
Tien Tran <tdtrantech@gmail.com>
2024-10-07 10:52:07 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=tien123435testUdacity
+export POSTGRES_HOST=database-1.c7eggku0wuah.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=tientranudacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
